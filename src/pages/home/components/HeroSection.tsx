b5566fe32964edc4ee88f7b5b55a823a367af327
Akhila <gantaakhila773@gmail.com>
2026-01-16 20:01:17 +0530

    Trigger redeploy

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -58,10 +58,10 @@ const HeroSection = () => {
              Welcome To Dew Dale Resorts
             </h3>
 
-
+{/* 
              <div className="w-full mt-6 px-2 sm:px-4 md:px-6">
                 <BookingBar />
-              </div>
+              </div> */}
 
             <div className="inner-block mt-6 flex flex-col sm:flex-row items-center justify-center gap-6">
               <motion.a
